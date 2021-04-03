f44b41a226889c42610cd2430197274f58e65329
shaalux <mhs002@usal.edu.lb>
2021-04-03 15:56:35 +0300

    Added the assignment file

---
+++ Assignment_1/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assignment_1.Models;
+
+namespace Assignment_1.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            List<Student> student = new List<Student>();
+            student.Add(new Student { StudentName="Mohammad",StudentId=2});
+            
+            return View(student.AsEnumerable());
+        }
+
+     
+    }
+}
---
+++ Assignment_1/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment_1.Models
+{
+    public class Student
+    {
+       
+
+     
+        public int StudentId {
+            get;
+            set;
+        }
+
+        public String StudentName {
+            get;
+            set;
+        }
+    }
+}
